b295b3db1eab2a479b9149c2b635906b9df16990
alonskaya <annalonskaya@mail.ru>
2017-02-14 15:39:58 +0200

    Исправлены замечания к заданию №2.

--- sandbox/src/main/java/ua/annalonskaya/sandbox/MyProgram.java
+++ sandbox/src/main/java/ua/annalonskaya/sandbox/MyProgram.java
@@ -1,9 +1,16 @@
 package ua.annalonskaya.sandbox;
 
+import static ua.annalonskaya.sandbox.Point.distance;
+
 public class MyProgram {
 
-	public static void main(String[] args) {
-		System.out.println("Hello, Anna");
-	}
+    public static void main(String[] args) {
+
+        Point p1 = new Point(2, 3);
+        Point p2 = new Point(1, 4);
+        System.out.println("Distance between two points p1(" + p1.x + ", " + p1.y + ") and p2(" + p2.x + ", " + p2.y + ") is " + distance(p1, p2));
+        System.out.println("Distance between two points p1(" + p1.x + ", " + p1.y + ") and p2(" + p2.x + ", " + p2.y + ") is " + p1.distance(p2));
+
+    }
 
 }
--- sandbox/src/main/java/ua/annalonskaya/sandbox/Point.java
+++ sandbox/src/main/java/ua/annalonskaya/sandbox/Point.java
@@ -5,24 +5,20 @@ package ua.annalonskaya.sandbox;
  */
 public class Point {
 
-  public double x;
-  public double y;
+    public double x;
+    public double y;
 
-  public Point(double x, double y) {
-    this.x = x;
-    this.y = y;
-  }
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
 
-  public static void main(String[] args) {
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+    }
 
-    Point p1 = new Point(2, 3);
-    Point p2 = new Point(1, 4);
-    System.out.println("Distance between two points p1(" + p1.x + ", " + p1.y + ") and p2(" + p2.x + ", " + p2.y + ") is " + distance(p1, p2));
-
-  }
-
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
-  }
+    public double distance(Point p2) {
+        return Math.sqrt((this.x - p2.x) * (this.x - p2.x) + (this.y - p2.y) * (this.y - p2.y));
+    }
 
 }
