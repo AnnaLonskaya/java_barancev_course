29d9ec89b75da78ff41f5a1196d8a7c484476b94
alonskaya <annalonskaya@mail.ru>
2017-02-14 15:51:53 +0200

    Small changes.

--- sandbox/src/main/java/ua/annalonskaya/sandbox/MyProgram.java
+++ sandbox/src/main/java/ua/annalonskaya/sandbox/MyProgram.java
@@ -6,8 +6,8 @@ public class MyProgram {
 
     public static void main(String[] args) {
 
-        Point p1 = new Point(2, 3);
-        Point p2 = new Point(1, 4);
+        Point p1 = new Point(4, 5);
+        Point p2 = new Point(2, 3);
         System.out.println("Distance between two points p1(" + p1.x + ", " + p1.y + ") and p2(" + p2.x + ", " + p2.y + ") is " + distance(p1, p2));
         System.out.println("Distance between two points p1(" + p1.x + ", " + p1.y + ") and p2(" + p2.x + ", " + p2.y + ") is " + p1.distance(p2));
 
