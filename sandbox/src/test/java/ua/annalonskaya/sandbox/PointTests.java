202c9b6db14228f8220767b86e75a6f1f2b129d7
AnnaLonskaya <annalonskaya@mail.ru>
2017-04-13 21:27:37 +0300

    Исправлен тест Point Tests.

--- sandbox/src/test/java/ua/annalonskaya/sandbox/PointTests.java
+++ sandbox/src/test/java/ua/annalonskaya/sandbox/PointTests.java
@@ -5,16 +5,13 @@ import org.testng.annotations.Test;
 
 import static ua.annalonskaya.sandbox.Point.distance;
 
-/**
- * Created by Admin on 16.02.2017.
- */
 public class PointTests {
 
   @Test
   public void testDistanceP1P2() {
     Point p1 = new Point(4, 5);
     Point p2 = new Point(2, 3);
-    Assert.assertEquals(distance(p1, p2), 2);
+    Assert.assertEquals(distance(p1, p2), 2.8284271247461903);
   }
 
   @Test
